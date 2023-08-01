9d68efc0b52974de448de8d9969f560d95dae6f3
Asmaa Adel <adelasmaa333@gmail.com>
2023-08-01 16:24:44 -0400

    create component part for the project

--- backend/app/app.go
+++ backend/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 
+	"github.com/codescalersinternships/Flyspray/handlers"
 	"github.com/codescalersinternships/Flyspray/models"
 	"github.com/gin-gonic/gin"
 )
@@ -33,6 +34,22 @@ func (a *App) Run(port int) error {
 	a.router = gin.Default()
 
 	// set routes here
+
+	component := a.router.Group("/component")
+	{
+		component.POST("/", func(c *gin.Context) {
+			handlers.CreateComponent(c, a.client)
+		})
+		component.GET("/:id", func(c *gin.Context) {
+			handlers.GetComponentByID(c, a.client)
+		})
+		component.DELETE("/:id", func(c *gin.Context) {
+			handlers.DeleteComponent(c, a.client)
+		})
+		component.GET("/filters", func(c *gin.Context) {
+			handlers.ListComponentsForProject(c, a.client)
+		})
+	}
 
 	return a.router.Run(fmt.Sprintf(":%d", port))
 }
