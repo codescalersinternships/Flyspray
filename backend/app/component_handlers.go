d96322b8b7469e164a593133f7a57b7a3c73f40e
Asmaa Adel <adelasmaa333@gmail.com>
2023-08-02 08:11:01 -0400

    update handlres and tests

--- backend/app/component_handlers.go
+++ backend/app/component_handlers.go
@@ -1,4 +1,4 @@
-package handlers
+package app
 
 import (
 	"fmt"
@@ -11,14 +11,14 @@ import (
 )
 
 // CreateComponent creates a new component
-func CreateComponent(c *gin.Context, db models.DBClient) {
+func (app *App) CreateComponent(c *gin.Context) {
 	var component models.Component
 	if err := c.ShouldBindJSON(&component); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	if result := db.Client.Create(&component); result.Error != nil {
+	if result := app.client.Client.Create(&component); result.Error != nil {
 		log.Fatal(result.Error)
 		c.Status(http.StatusInternalServerError)
 		return
@@ -28,11 +28,11 @@ func CreateComponent(c *gin.Context, db models.DBClient) {
 }
 
 // GetComponentByID gets a component by its ID
-func GetComponentByID(c *gin.Context, db models.DBClient) {
+func (app *App) GetComponentByID(c *gin.Context) {
 
 	componentID := c.Param("id")
 	component := models.Component{}
-	result := db.Client.First(&component, componentID)
+	result := app.client.Client.First(&component, componentID)
 
 	if result.Error != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Component not found"})
@@ -42,16 +42,16 @@ func GetComponentByID(c *gin.Context, db models.DBClient) {
 }
 
 // DeleteComponent deletes a component by its ID
-func DeleteComponent(c *gin.Context, db models.DBClient) {
+func (app *App) DeleteComponent(c *gin.Context) {
 	componentID := c.Param("id")
 
 	component := models.Component{}
 
-	if result := db.Client.First(&component, componentID); result.Error != nil {
+	if result := app.client.Client.First(&component, componentID); result.Error != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Component not found"})
 		return
 	}
-	if result := db.Client.Delete(&component, componentID); result.Error != nil {
+	if result := app.client.Client.Delete(&component, componentID); result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete the component"})
 		return
 	}
@@ -60,8 +60,9 @@ func DeleteComponent(c *gin.Context, db models.DBClient) {
 }
 
 // ListComponentsForProject gets all components for a project
-func ListComponentsForProject(c *gin.Context, db models.DBClient) {
+func (app *App) ListComponentsForProject(c *gin.Context) {
 	projectID := c.Query("project_id")
+	name := c.Query("name")
 
 	fmt.Println(projectID)
 	if projectID == "" {
@@ -70,8 +71,14 @@ func ListComponentsForProject(c *gin.Context, db models.DBClient) {
 		return
 	}
 
+	query := app.client.Client.Where("project_id = ?", projectID)
+
+	if name != "" {
+		query = query.Where("name = ?", name)
+	}
+
 	components := []models.Component{}
-	db.Client.Where("project_id = ?", projectID).Find(&components)
+	query.Find(&components)
 
 	if len(components) == 0 {
 		c.JSON(http.StatusNotFound, gin.H{"message": "No components found for the specified project_id"})
