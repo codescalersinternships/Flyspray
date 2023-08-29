23503092588be19593049872c636d4a9460c38b0
Rodina Mohamed <75133923+Rodina222@users.noreply.github.com>
2023-08-29 15:31:52 +0300

    add_version_commit_cmd

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/codescalersinternships/Flyspray/app"
 	_ "github.com/codescalersinternships/Flyspray/docs"
 )
 
+var (
+	version string
+	commit  string
+)
+
 func main() {
+
+	fmt.Println("Version:", version)
+	fmt.Println("Commit:", commit)
+
 	var configFilePath string
 	flag.StringVar(&configFilePath, "f", "config.json", "Specify the filepath of json configuration file")
 	flag.Parse()
