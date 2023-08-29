b4232b87c52feb56ec92d825649e804945bfd57e
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-08-29 19:20:38 +0300

    create home view for not logged in user

---
+++ frontend/src/components/home/HomeNotLoggedinComponent.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <v-sheet width="400" class="mx-auto">
+      <p class="header-text">Welcome to FlySpray</p>
+      <div class="container">
+        <p class="sub-header-text">
+          Flyspray is an open-source web-based project management and bug
+          tracking system. It provides a platform for teams to collaborate,
+          track tasks, and manage software development projects. With features
+          such as task assignment, progress tracking, user permissions, Flyspray
+          streamlines project communication and helps teams identify and resolve
+          software issues efficiently. It is a tool commonly used in software
+          development and other project-based industries to enhance teamwork and
+          project organization.
+        </p>
+      </div>
+      <div class="buttons">
+        <v-btn type="submit" block class="mt-2 btn" @click="goToRegisterPage()"
+          >Register</v-btn
+        >
+        <v-btn type="submit" block class="mt-2 btn" @click="goToLoginPage()"
+          >Sign In</v-btn
+        >
+      </div>
+    </v-sheet>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  methods: {
+    goToRegisterPage() {
+      this.$router.push("/signup");
+    },
+    goToLoginPage() {
+      this.$router.push("/login");
+    },
+  },
+});
+</script>
+
+<style scoped>
+.header-text {
+  color: #6945c4;
+  text-align: center;
+  font-family: Poppins;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-transform: capitalize;
+  margin-bottom: 2rem;
+}
+.sub-header-text {
+  font-size: 1rem;
+  margin-bottom: 1rem;
+  font-weight: 400;
+  color: #525252;
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  text-align: left;
+  margin-bottom: 1rem;
+}
+.buttons {
+  width: 70%;
+  text-align: center;
+  align-items: center;
+  margin: auto;
+  margin-top: 1.5rem;
+}
+.btn {
+  border-radius: 8px;
+  background: linear-gradient(134deg, #9181f4 0%, #5038ed 100%);
+  box-shadow: 0px 8px 21px 0px rgba(0, 0, 0, 0.16);
+  color: #ffff;
+  margin-bottom: 1.5rem;
+}
+</style>
---
+++ frontend/src/views/home/HomeNotLoggedinView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row">
+    <logo-image class="child1"></logo-image>
+    <div class="child2">
+      <home-not-loggedin-component></home-not-loggedin-component>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import HomeNotLoggedinComponent from "../../components/home/HomeNotLoggedinComponent.vue";
+import LogoImage from "../../components/LogoImage.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoginPage",
+  components: {
+    HomeNotLoggedinComponent,
+    LogoImage,
+  },
+});
+</script>
+<style scoped>
+.row {
+  display: flex;
+  height: auto;
+  min-height: 100vh;
+}
+.child1 {
+  flex-basis: 50%;
+  width: 50%;
+  text-align: center;
+  background-color: #8457f7;
+}
+.child2 {
+  flex-basis: 50%;
+  width: 50%;
+  text-align: center;
+  margin-top: 5rem;
+}
+
+@media (max-width: 1100px) {
+  .child1 {
+    display: none;
+  }
+  .child2 {
+    flex-basis: 100%;
+    width: auto;
+    padding: 0;
+  }
+}
+</style>
