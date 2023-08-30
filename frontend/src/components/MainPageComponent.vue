f247cf3ede7f30058002414e1354b64ae87e82fe
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-08-30 21:49:28 +0300

    update home page

--- frontend/src/components/MainPageComponent.vue
+++ frontend/src/components/MainPageComponent.vue
@@ -1,11 +1,15 @@
 <template>
   <v-col id="main-section-container" class="h-100" cols="8">
     <div id="main-section-innerdiv">
-      <h2>Main Section</h2>
+      <div class="header"><v-icon>mdi-home-outline</v-icon> Projects</div>
     </div>
   </v-col>
 </template>
 <style scoped>
+.header {
+  font-weight: 400;
+  font-size: 1.3rem;
+}
 #main-section-container {
   display: flex;
 }
@@ -13,7 +17,7 @@
 #main-section-innerdiv {
   width: 100%;
   border-radius: 4px;
-  border: 1px solid var(--main-color, #8457f7);
+  /* border: 1px solid var(--main-color, #8457f7); */
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 }
 </style>
