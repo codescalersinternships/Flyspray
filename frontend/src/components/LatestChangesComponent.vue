c45ec0b5f5be3be2dc29f00e425b46c5165b1500
Hanya-Ahmad <hanya.taweel01@eng-st.cu.edu.eg>
2023-08-30 13:55:47 +0300

    added latest-changes sidebar

---
+++ frontend/src/components/LatestChangesComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-col id="latest-changes-container" class="h-100" cols="4">
+    <div id="latest-changes-innerdiv">
+      <div id="latest-changes-title">Latest Changes</div>
+      <div id="vertical-line">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="2"
+          height="100%"
+          viewBox="0 0 2 100%"
+          fill="none"
+        >
+          <path d="M1 0L0.999982 404" stroke="#BAA2F9" />
+        </svg>
+        <div class="circle-container">
+          <template v-for="index in 6" :key="index">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+            >
+              <circle cx="7.5" cy="7.5" r="7.5" fill="#BAA2F9" />
+            </svg>
+          </template>
+        </div>
+      </div>
+    </div>
+  </v-col>
+</template>
+
+<style scoped>
+#latest-changes-container {
+  display: flex;
+  align-items: flex-start;
+}
+#latest-changes-innerdiv {
+  width: 100%;
+  height: 75%;
+  border-radius: 4px;
+  border: 1px solid var(--light-purple, #baa2f9);
+  position: relative;
+}
+#latest-changes-title {
+  text-align: start;
+  padding-top: 16px;
+  padding-left: 16px;
+}
+#vertical-line {
+  position: absolute;
+  left: 16px;
+  padding-top: 30px;
+  height: 80%;
+}
+.circle-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: absolute;
+  top: 30px;
+  left: -5px;
+  height: calc(100% - 30px);
+}
+</style>
+<script lang="ts">
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
--- frontend/src/views/HomeLoggedInView.vue
+++ frontend/src/views/HomeLoggedInView.vue
@@ -1,15 +1,39 @@
 <template>
-  <div ud="navbar">
+  <v-app>
     <navbar-sidebar-component></navbar-sidebar-component>
-  </div>
+    <v-main class="main-page">
+      <v-container class="h-100">
+        <v-row class="h-100">
+          <main-page-component></main-page-component>
+          <latest-changes-component></latest-changes-component>
+        </v-row>
+        <footer-component></footer-component>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+<style scoped>
+.main-page {
+  background: #161616;
+  color: #fff;
+  font-family: Roboto;
+  font-style: normal;
+  line-height: normal;
+}
+</style>
 <script lang="ts">
 import NavbarSidebarComponent from "@/components/NavbarSidebarComponent.vue";
+import MainPageComponent from "@/components/MainPageComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import LatestChangesComponent from "@/components/LatestChangesComponent.vue";
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "HomeLoggedInPage",
   components: {
     NavbarSidebarComponent,
+    MainPageComponent,
+    FooterComponent,
+    LatestChangesComponent,
   },
 });
 </script>
